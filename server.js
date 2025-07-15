5b99d140c21c6e3523cbc0d80e481567c6c5454c
Mordraga <sarynharris2021@gmail.com>
2025-07-15 03:27:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.post('/api/chat', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('TagSpeak Proxy Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`TagSpeak Proxy Server running on port ${PORT}`);
 });
+
